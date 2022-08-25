e2e6725ee50e03a39c1334d8d13adf8f434f10a8
wangxingkang <wang_xingkang@qq.com>
2022-08-25 11:58:38 +0800

    feat(react): add getTargetElement function

--- .redbudrc.ts
+++ .redbudrc.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'redbud';
 
 export default defineConfig({
-  esm: {},
-  cjs: {},
+  esm: {
+    output: 'lib',
+  },
+  cjs: {
+    output: 'lib/_cjs',
+  },
+  platform: 'browser',
 });
