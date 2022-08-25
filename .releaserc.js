10aea840569809723c4b046be8a53d50006b2688
wangxingkang <wang_xingkang@qq.com>
2022-08-26 00:21:58 +0800

    chore: test

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['+([0-9])?(.{+([0-9]),x}).x", "main'],
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main'],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
@@ -11,7 +11,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        "assets": ['CHANGELOG.md', 'package.json'],
+        assets: ['CHANGELOG.md', 'package.json'],
         message:
           // eslint-disable-next-line no-template-curly-in-string
           'chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
