337e8bfb8b5f79fe2331cdd29327ad9962f1cd85
wangxingkang <wang_xingkang@qq.com>
2023-10-19 14:01:48 +0800

    test(utils): add uuid test

---
+++ src/utils/uuid.test.ts
@@ -0,0 +1,14 @@
+import { uuid } from './uuid';
+
+describe(uuid.name, () => {
+  test('返回值为字符串', () => {
+    expect(typeof uuid()).toBe('string');
+  });
+
+  test('多次调用返回值不同', () => {
+    const id1 = uuid();
+    const id2 = uuid();
+
+    expect(id1 === id2).toBeFalsy();
+  });
+});
